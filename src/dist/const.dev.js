7193848c60231c5d5fda261b69cd331cbca4adfa
Super3laa <superalaa28@gmail.com>
2021-12-29 19:47:00 +0200

    HotFixv0.01

--- src/dist/const.dev.js
+++ src/dist/const.dev.js
@@ -1,3 +1,9 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.API = void 0;
 var API = "http://164.90.164.236:5050/"; //export const API = "http://localhost:9000/"
+
+exports.API = API;
